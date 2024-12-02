f7aa128a054f5efffb8fb491f4100b4e9bd28953
punkjoker <107567875+punkjoker@users.noreply.github.com>
2024-12-02 10:35:29 +0300

    git commit

--- includes/stk_initiate.php
+++ includes/stk_initiate.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://github.com/punkjoker/daraja-API.git';  
+  $CallBackURL = 'https://github.com/punkjoker/sunflower-cafe.git';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
